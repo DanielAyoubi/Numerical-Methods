58462cbc350a9e4d7ac73f07797a177fd2ace0ce
DanielAyoubi <danielayoubi@hotmail.com>
2023-02-19 18:54:02 +0100

    Exercise complex

--- exercises/complex/main.cs
+++ exercises/complex/main.cs
@@ -7,19 +7,18 @@ class Program{
 
     public static void Main(){
 
-		complex num = new complex(-1,0);
-  		Complex imag = Complex.Sqrt(-1);
+  		/* Complex imag = Complex.Sqrt(-1); */
+		/* Complex I = Complex.Sqrt(imag); */
+		/* Complex E_i_pi = Complex.Exp(imag * Math.PI); */
+        /* Complex E_I = Complex.Exp(imag); */
 
-		complex i = cmath.sqrt(num);     // √-1
-		Complex I = Complex.Sqrt(imag);
+		complex i = cmath.sqrt(new complex(-1,0));     // √-1
 
 		complex sqrt_i = cmath.sqrt(i);  // √i
-		
+        		
 		complex e_i = cmath.exp(i);      // e^i
-		Complex E_I = Complex.Exp(imag);
 		
 		complex e_i_pi = cmath.exp(i * Math.PI);   // e^iπ
-		Complex E_i_pi = Complex.Exp(imag * Math.PI);
 		
 		complex i_i = cmath.pow(i, i);
 
@@ -28,9 +27,9 @@ class Program{
 		complex sin_i = cmath.sin(i * Math.PI);
 
 
-		WriteLine($"√-1 = {num} \n√i = {sqrt_i} \ne^i = {e_i}  \ne^iπ = {e_i_pi} \ni^i = {i_i} \nln(i) = {ln_i} \nsin(iπ) = {sin_i}");
+		WriteLine($"√-1 = {i} \n√i = {sqrt_i} \ne^i = {e_i}  \ne^iπ = {e_i_pi} \ni^i = {i_i} \nln(i) = {ln_i} \nsin(iπ) = {sin_i}");
 	
 		WriteLine(complex.approx(Math.Sqrt(-1), i));
-		WriteLine(complex.approx(new complex(0,1), cmath.log(i)));
+		WriteLine(complex.approx(cmath.log(i), cmath.log(i)));
     }
 }
